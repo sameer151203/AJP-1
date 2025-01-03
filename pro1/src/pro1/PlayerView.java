1fb7958050fbd05fea104ef76fe9e3e15f6416b1
sameer151203 <sameer151203@gmail.com>
2025-01-03 10:57:27 +0530

    Add files via upload

---
+++ pro1/src/pro1/PlayerView.java
@@ -0,0 +1,20 @@
+package pro1;
+
+public class PlayerView {
+	
+	    public void displayPlayerDetails(Player player) {
+	        System.out.println("Player Details:");
+	        System.out.println("ID: " + player.getId());
+	        System.out.println("Name: " + player.getName());
+	        System.out.println("Team: " + player.getTeam());
+	        System.out.println("Role: " + player.getRole());
+	        System.out.println("Runs: " + player.getRuns());
+	        System.out.println("Wickets: " + player.getWickets());
+	       
+	    }
+
+	    public void displayTopPlayer(String title, Player player) {
+	        System.out.println(title + ":");
+	        displayPlayerDetails(player);
+	    }
+	}
